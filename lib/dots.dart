49adb1840f2dced823fd8411f53bdbaf2e3bd1b4
antonionicolau <antonio.tioypedro1234@gmail.com>
2023-10-06 10:20:35 +0100

    updated dot to align based on scrollDirection

--- lib/dots.dart
+++ lib/dots.dart
@@ -1,29 +1,41 @@
 import 'package:flutter/material.dart';
-
 import 'dot.dart';
 
 class Dots extends StatelessWidget {
-  const Dots(
-      {Key? key,
-      required this.totalSlides,
-      required this.dotsCurve,
-      this.height,
-      this.width})
-      : super(key: key);
+  const Dots({
+    Key? key,
+    required this.totalSlides,
+    required this.dotsCurve,
+    required this.scrollDirection,
+    this.height,
+    this.width,
+  }) : super(key: key);
   final int totalSlides;
   final Curve dotsCurve;
   final double? height;
   final double? width;
+  final Axis scrollDirection;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: width ?? double.infinity,
-      height: height ?? 70,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: List.generate(
-            totalSlides, (index) => Dot(index: index, dotsCurve: dotsCurve)),
-      ),
+      width: width,
+      height: scrollDirection == Axis.horizontal ? 70 : null,
+      child: scrollDirection == Axis.horizontal
+          ? Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(totalSlides,
+                  (index) => Dot(index: index, dotsCurve: dotsCurve)),
+            )
+          : Column(
+              mainAxisSize: MainAxisSize.min,
+              children: List.generate(
+                  totalSlides,
+                  (index) => Dot(
+                        index: index,
+                        dotsCurve: dotsCurve,
+                        scrollDirection: scrollDirection,
+                      )),
+            ),
     );
   }
 }
