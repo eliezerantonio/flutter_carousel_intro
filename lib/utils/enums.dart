71140d7f04fcfd2d36280b18011a3a25dd98fc4b
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-10-07 01:54:42 +0100

    refactor: remove unnecessary import

--- lib/utils/enums.dart
+++ lib/utils/enums.dart
@@ -1,5 +1,3 @@
-import 'package:flutter_carousel_intro/page_indicator/effects/worm_effect.dart';
-
 enum IndicatorAlign { top, bottom, left, right }
 
 enum IndicatorEffects {
@@ -12,4 +10,3 @@ enum IndicatorEffects {
   slide,
   swap,
 }
-
