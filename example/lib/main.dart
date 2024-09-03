03046bc0112c29dcfd4fc17d7ba2bdf5d608bd9d
ildeberto <ildeberto34@gmail.com>
2024-09-03 01:51:16 +0100

    upgrade sdk

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Slide Show Demo',
       theme: ThemeData(useMaterial3: true),
       debugShowCheckedModeBanner: false,
       home: const MyHomePage(),
